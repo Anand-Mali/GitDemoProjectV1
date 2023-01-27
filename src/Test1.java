4148da76c5c829ad07ded15312d54dbc2e4b3e78
anand <anandmali1890@gmail.com>
2023-01-27 22:20:08 +0530

    "This is my demo first commit"

---
+++ src/Test1.java
@@ -0,0 +1,14 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+
+		int age =19;
+		if(age == 19) {
+			System.out.println("adult");
+		}else {
+			System.out.println("Not adult");
+		}
+	}
+
+}
